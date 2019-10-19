998c047ec0ea8ac93d307ddc667ed81a5e87435d
jiaqihuang-233 <jhuang@pdftron.com>
2019-10-18 17:44:55 -0700

    JH: disabled unused annotation buttons

--- src/Drawer.js
+++ src/Drawer.js
@@ -9,15 +9,42 @@ class Drawer extends React.Component {
       this.instance = null;
     }
   
-      componentDidMount() {
-        
+      async componentDidMount() {
+        const instance = await window.WebViewer({
+            path: '/lib',
+            initialDoc: '/files/bgimg.png'
+        }, this.viewer.current);
+
+        this.instance = instance;
+        this.docViewer = instance.docViewer;
+        this.annotManager = instance.annotManager;
+        instance.setAdminUser(true);
+        instance.setAnnotationUser(this.props.user);
+
+        //disable other anno tools except for freehand
+        instance.setHeaderItems( header => {
+            const items = header.getItems();
+            const removed = items.filter(item => !(item.type === 'toolButton' 
+                                                    || item.type === 'statefulButton'
+                                                    || (item.type === 'toolGroupButton' && item.toolGroup !== 'freeHandTools')));
+            console.log(removed);
+            header.update(removed);
+          });
+
       }
+
+      //listener for drawing
+
+      //update timstamp whenever drawing is done.
+
+      //add a save button, see https://www.pdftron.com/documentation/web/guides/customizing-header?searchTerm=disable#toolbutton
   
       render() {
           return (
-            <div>
-                <h1>{this.props.user}</h1>
-            </div>
+            <div className="App">
+            <div className="header">{this.props.user}</div>
+            <div className="webviewer" ref={this.viewer}></div>
+          </div>
           );
       }  
 }
