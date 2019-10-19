f192eff3ed3bc0b7ed5cb3ef190ddd898c72dee4
jiaqihuang-233 <jhuang@pdftron.com>
2019-10-18 17:58:16 -0700

    JH: added save button

--- src/Drawer.js
+++ src/Drawer.js
@@ -27,17 +27,24 @@ class Drawer extends React.Component {
             const removed = items.filter(item => !(item.type === 'toolButton' 
                                                     || item.type === 'statefulButton'
                                                     || (item.type === 'toolGroupButton' && item.toolGroup !== 'freeHandTools')));
-            console.log(removed);
-            header.update(removed);
+            const saveButton = {
+                type: 'actionButton',
+                img: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M17 3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V7l-4-4zm-5 16c-1.66 0-3-1.34-3-3s1.34-3 3-3 3 1.34 3 3-1.34 3-3 3zm3-10H5V5h10v4z"/></svg>',
+                onClick: function() {
+                  /**@TODO save the annotations */
+                }
+            };
+            const updatedButtons = [...removed, saveButton];
+            console.log(updatedButtons);
+            header.update(updatedButtons);
           });
 
       }
 
       //listener for drawing
 
-      //update timstamp whenever drawing is done.
+      //update timstamp whenever drawing is done. can use Moment.js to display time stamp
 
-      //add a save button, see https://www.pdftron.com/documentation/web/guides/customizing-header?searchTerm=disable#toolbutton
   
       render() {
           return (
